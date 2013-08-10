e0cc3746f789a258bc884f7535d0f4555e050e31
tsapeta <tomasz.sapeta@me.com>
2013-08-10 22:14:09 +0200

    increased version to 0.3.2

--- CodePrinter.js
+++ CodePrinter.js
@@ -32,7 +32,7 @@ window.CodePrinter = (function($) {
         return self;
     };
     
-    CodePrinter.version = '0.3.1';
+    CodePrinter.version = '0.3.2';
     
     CodePrinter.defaults = {
         path: '',
