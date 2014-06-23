fa784ac0e5340576f663ef837311008c72ba445b
tsapeta <tomasz.sapeta@me.com>
2014-06-23 23:53:15 +0200

    changes in createHighlightOverlay

--- CodePrinter.js
+++ CodePrinter.js
@@ -964,10 +964,10 @@ loader(function($) {
             if (this.highlightOverlay) this.highlightOverlay.remove();
             var self = this, args = arguments
             , overlay = this.highlightOverlay = new CodePrinter.Overlay(this, 'cp-highlight-overlay', false);
-            overlay.on('refresh', function(a) { a == null && self.createHighlightOverlay.apply(self, args); });
+            overlay.on('refresh', function(a) { a == null ? self.createHighlightOverlay.apply(self, args) : a == 'blur' && overlay.remove(); });
             for (var i = 0; i < arguments.length; i++) {
                 var pos = getPositionOf(this, arguments[i][0], arguments[i][1]);
-                overlay.node.append(createSpan(arguments[i][2], 'cp-highlight', pos.y, pos.x, arguments[i][2].length * this.sizes.charWidth, this.sizes.lineHeight));
+                overlay.node.append(createSpan(arguments[i][2], 'cp-highlight', pos.y, pos.x, arguments[i][2].length * this.sizes.charWidth + 1, this.sizes.lineHeight));
             }
             overlay.reveal();
             return this;
