9554dc28a2f9c5c6a71cb93d2be113eb385ce759
tsapeta <tomasz.sapeta@me.com>
2014-07-21 21:35:22 +0200

    update demo

--- demo/script.js
+++ demo/script.js
@@ -1,4 +1,17 @@
-var cp = new CodePrinter();
+var cp = new CodePrinter()
+, demo = $.get('[name=demo]')
+, theme = $.get('[name=theme]')
+, caretStyle = $.get('[name=caretStyle]')
+, tabWidth = $.get('[name=tabWidth]')
+, fontSize = $.get('[name=fontSize]')
+, fullscreen = $.get('[name=fullscreen]')
+, counter = $.get('[name=counter]')
+, insertclosing = $.get('[name=insertclosing]')
+, indentation = $.get('[name=indentation]')
+, indentNewLines = $.get('[name=indentNewLines]')
+, highlightBrackets = $.get('[name=highlightBrackets]')
+, shortcuts = $.get('[name=shortcuts]')
+, blinkCaret = $.get('[name=blinkCaret]');
 
 $.get('section').append(cp.mainElement);
 cp.print('JavaScript', $.get('code#JavaScript').innerHTML.decode());
@@ -13,58 +26,44 @@ setTimeout(function() {
     });
 }, 150);
 
-$.get('[name=demo]')
-.on({ change: function() {
+demo.onchange = function() {
     cp.print(this.value, $.get('code#'+this.value).innerHTML.decode());
-}});
-$.get('[name=theme]').prop({ value: cp.options.theme })
-.on({ change: function() {
+}
+theme.onchange = function() {
     cp.setTheme(this.value);
-}});
-$.get('[name=caretStyle]').prop({ value: cp.options.caretStyle })
-.on({ change: function() {
+}
+caretStyle.onchange = function() {
     cp.caret.setStyle(this.value);
-}});
-$.get('[name=tabWidth]').prop({ value: cp.options.tabWidth })
-.on({ change: function() {
+}
+tabWidth.onchange = function() {
     cp.setTabWidth(parseInt(this.value));
     this.focus();
-}});
-$.get('[name=fontSize]').prop({ value: cp.options.fontSize })
-.on({ change: function() {
+}
+fontSize.onchange = function() {
     cp.setFontSize(this.value);
     this.focus();
-}});
-$.get('[name=fullscreen]')
-.on({ click: function() {
+}
+fullscreen.onclick = function() {
     cp.enterFullscreen();
-}});
-$.get('[name=counter]').prop({ checked: cp.options.lineNumbers })
-.on({ change: function() {
+}
+counter.onchange = function() {
     this.checked ? cp.openCounter() : cp.closeCounter();
-}});
-$.get('[name=insertclosing]').prop({ checked: cp.options.insertClosingBrackets })
-.on({ change: function() {
+}
+insertclosing.onchange = function() {
     cp.options.insertClosingBrackets = this.checked;
-}});
-$.get('[name=indentation]').prop({ checked: cp.options.showIndentation })
-.on({ change: function() {
-    cp.options.showIndentation = this.checked;
-    cp.forcePrint();
-}});
-$.get('[name=indentNewLines]').prop({ checked: cp.options.indentNewLines })
-.on({ change: function() {
+}
+indentation.onchange = function() {
+    this.checked ? cp.showIndentation() : cp.hideIndentation();
+}
+indentNewLines.onchange = function() {
     cp.options.indentNewLines = this.checked;
-}});
-$.get('[name=highlightBrackets]').prop({ checked: cp.options.highlightBrackets })
-.on({ change: function() {
+}
+highlightBrackets.onchange = function() {
     cp.options.highlightBrackets = this.checked;
-}});
-$.get('[name=shortcuts]').prop({ checked: cp.options.shortcuts })
-.on({ change: function() {
+}
+shortcuts.onchange = function() {
     cp.options.shortcuts = this.checked;
-}});
-$.get('[name=blinkCaret]').prop({ checked: cp.options.blinkCaret })
-.on({ change: function() {
+}
+blinkCaret.onchange = function() {
     cp.options.blinkCaret = this.checked;
-}});
+}
