8a1751522365a02c897738492f93dc1b92f8efb9
tsapeta <tomasz.sapeta@me.com>
2015-03-17 20:52:31 +0100

    update css-colors addon

--- addons/css-colors.js
+++ addons/css-colors.js
@@ -1,28 +1,31 @@
 CodePrinter.defineAddon('css-colors', function() {
+  
+  function is(node, className) {
+    return node.className.indexOf(className) >= 0;
+  }
+  
   return function(cp, options) {
     var prop = options && options.property || 'color';
     
-    cp.wrapper.delegate('span.cpx-hex', {
-      mouseover: function() {
-        var html = this.innerHTML;
-        if (/^#([0-9a-f]{3}|[0-9a-f]{6})$/i.test(html)) {
-          this.style[prop] = html;
+    cp.wrapper.addEventListener('mouseover', function(e) {
+      var target = e.target;
+      if (target.tagName == 'SPAN') {
+        if (is(target, 'cpx-hex')) {
+          var html = target.innerHTML;
+          if (/^#([0-9a-f]{3}|[0-9a-f]{6})$/i.test(html)) {
+            target.style[prop] = html;
+          }
+        } else if (is(target, 'cpx-css-color')) {
+          var html = target.innerHTML;
+          if (/^[a-z\-]+$/i.test(html)) {
+            target.style[prop] = html;
+          }
         }
-      },
-      mouseout: function() {
-        this.style.removeProperty(prop);
       }
     });
-    
-    cp.wrapper.delegate('span.cpx-css-color', {
-      mouseover: function() {
-        var html = this.innerHTML;
-        if (/^[a-z\-]+$/i.test(html)) {
-          this.style[prop] = html;
-        }
-      },
-      mouseout: function() {
-        this.style.removeProperty(prop);
+    cp.wrapper.addEventListener('mouseout', function(e) {
+      if (e.target.tagName == 'SPAN' && (is(e.target, 'cpx-hex') || is(e.target, 'cpx-hex'))) {
+        e.target.style.removeProperty(prop);
       }
     });
   }
