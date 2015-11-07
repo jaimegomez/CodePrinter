9ba5dbfa7bc3077878aaa1d8fb6f1ea4ae517891
tsapeta <tomasz.sapeta@me.com>
2015-11-07 18:46:19 +0100

    [CSS] add more tags

--- mode/CSS.js
+++ mode/CSS.js
@@ -9,12 +9,17 @@ CodePrinter.defineMode('CSS', function() {
   , pushIterator = CodePrinter.helpers.pushIterator
   , popIterator = CodePrinter.helpers.popIterator
   , tags = CodePrinter.keySet([
-    'html','body','div','a','ol','ul','li','span','p',
-    'h1','h2','h3','h4','h5','h6','img','input','textarea',
-    'button','form','label','select','option','optgroup',
-    'main','nav','header','section','aside','footer','code',
-    'fieldset','article','pre','table','tr','th','td',
-    'thead','tbody','tfoot','frameset','frame','iframe'
+    'a','abbr','address','area','article','aside','audio','b','base','bdi','bdo',
+    'bgsound','blockquote','body','br','button','canvas','caption','cite','code',
+    'col','colgroup','data','datalist','dd','del','details','dfn','div','dl','dt',
+    'em','embed','fieldset','figcaption','figure','footer','form','h1','h2','h3',
+    'h4','h5','h6','head','header','hgroup','hr','html','i','iframe','img','input',
+    'ins','kbd','keygen','label','legend','li','link','main','map','mark','marquee',
+    'menu','menuitem','meta','meter','nav','nobr','noframes','noscript','object',
+    'ol','optgroup','option','output','p','param','pre','progress','q','rp','rt',
+    'ruby','s','samp','script','section','select','small','source','span','strong',
+    'style','sub','summary','sup','table','tbody','td','textarea','tfoot','th','thead',
+    'time','tr','track','u','ul','var','video'
   ])
   , colors = [
     'inherit', 'transparent', 'aliceblue', 'antiquewhite', 'aqua', 'aquamarine', 'azure',
