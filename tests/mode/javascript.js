a31bc0fe1974a29f61dd839ac7802351f0ef02ad
tsapeta <tomasz.sapeta@me.com>
2016-04-16 23:58:13 +0200

    [js] more tests

--- tests/mode/javascript.js
+++ tests/mode/javascript.js
@@ -7,51 +7,118 @@ describe('JavaScript', function() {
     'return (n * factorial(n - 1));',
     '}',
     '',
-    '     console.log(factorial(7)); // 5040'
+    '     console.log(factorial(7)); // 5040',
+    '             {',
+    '  function tag(strings, ...values) {',
+    'return "Bazinga!";',
+    '}',
+    'let a = 5;',
+    'const b = 10;',
+    ' tag`Hello ${a + b} + ${"world" + b} ${a * b}!\\n`;',
+    '{',
+    '    let arrow = (a, ...b, c) => b.concat(a, c);',
+    '}',
+    '}',
+    '',
   ];
-  
+
   var doc = cp.createDocument(lines.join('\n'), 'JavaScript');
-  
+
   beforeAll(function() {
     cp.setDocument(doc);
   });
-  
-  it('should reindent', function() {
-    cp.exec('reindent');
+
+  it('should be initiated', function() {
     expect(cp.doc).toBe(doc);
     expect(doc.parser.name).toBe('javascript');
-    expect(doc.getIndent(0)).toBe(0);
-    expect(doc.getIndent(1)).toBe(1);
-    expect(doc.getIndent(2)).toBe(2);
-    expect(doc.getIndent(3)).toBe(1);
-    expect(doc.getIndent(4)).toBe(1);
-    expect(doc.getIndent(5)).toBe(0);
-    expect(doc.getIndent(6)).toBe(0);
-    expect(doc.getIndent(7)).toBe(0);
-    expect(doc.getIndent(8)).toBe(0); // this line doesn't exist
   });
-  
+
+  it('should reindent', function() {
+    var indents = [
+      0, 1, 2, 1, 1, 0, 0, 0, 0, 1,
+      2, 1, 1, 1, 1, 1, 2, 1, 0, 0
+    ];
+    cp.exec('reindent');
+    for (var i = 0; i < indents.length; i++) {
+      expect(doc.getIndent(i)).toBe(indents[i]);
+    }
+    expect(doc.getIndent(lines.length)).toBe(0); // this line doesn't exist
+  });
+
   it('should have correct contexts', function() {
     var parser = doc.parser;
-    
-    expect(doc.getState([0, 0]).state.context.type).toBe(parser.BLOCK_CONTEXT);
-    expect(doc.getState([0, 19]).state.context.type).toBe(parser.FUNCTION_CONTEXT);
-    expect(doc.getState([1, 3]).state.context.type).toBe(parser.FUNCTION_CONTEXT);
-    expect(doc.getState([1, 16]).state.context.type).toBe(parser.BLOCK_CONTEXT);
-    expect(doc.getState([3, 3]).state.context.type).toBe(parser.FUNCTION_CONTEXT);
-    expect(doc.getState([4, 10]).state.context.type).toBe(parser.FAKE_CONTEXT);
-    expect(doc.getState([4, 28]).state.context.type).toBe(parser.FAKE_CONTEXT);
-    expect(doc.getState([4, 30]).state.context.type).toBe(parser.FAKE_CONTEXT);
-    expect(doc.getState([4, 31]).state.context.type).toBe(parser.FUNCTION_CONTEXT);
-    expect(doc.getState([5, 1]).state.context.type).toBe(parser.BLOCK_CONTEXT);
+    var data = [
+      [0, 0, parser.BLOCK_CONTEXT],
+      [0, 19, parser.FUNCTION_CONTEXT],
+      [1, 3, parser.FUNCTION_CONTEXT],
+      [1, 16, parser.BLOCK_CONTEXT],
+      [3, 3, parser.FUNCTION_CONTEXT],
+      [4, 10, parser.FAKE_CONTEXT],
+      [4, 28, parser.FAKE_CONTEXT],
+      [4, 30, parser.FAKE_CONTEXT],
+      [4, 31, parser.FUNCTION_CONTEXT],
+      [5, 1, parser.BLOCK_CONTEXT],
+      [8, 1, parser.BLOCK_CONTEXT],
+      [10, 11, parser.FUNCTION_CONTEXT],
+      [15, 3, parser.BLOCK_CONTEXT],
+      [16, 32, parser.ARROW_CONTEXT],
+      [16, 47, parser.BLOCK_CONTEXT],
+    ];
+
+    for (var i = 0; i < data.length; i++) {
+      var item = data[i];
+      expect(doc.getState([item[0], item[1]]).state.context.type).toBe(item[2]);
+    }
   });
-  
+
   it('should recognize variables', function() {
-    expect(doc.hasSymbolAt('parameter', [0, 20])).toBeTruthy();
     expect(doc.hasSymbolAt('variable', [1, 7])).toBeTruthy();
     expect(doc.hasSymbolAt('variable', [4, 25])).toBeTruthy();
+    expect(doc.hasSymbolAt('variable', [14, 15])).toBeTruthy();
+    expect(doc.hasSymbolAt('variable', [14, 41])).toBeTruthy();
   });
-  
+
+  it('should recognize function parameters', function() {
+    expect(doc.hasSymbolAt('parameter', [0, 20])).toBeTruthy();
+    expect(doc.hasSymbolAt('parameter', [9, 22])).toBeTruthy();
+    expect(doc.hasSymbolAt('parameter', [9, 30])).toBeTruthy();
+    expect(doc.hasSymbolAt('parameter', [16, 24])).toBeTruthy();
+    expect(doc.hasSymbolAt('parameter', [16, 27])).toBeTruthy();
+  });
+
+  it('should recognize constants', function() {
+    expect(doc.hasSymbolAt('constant', [13, 9])).toBeTruthy();
+    expect(doc.hasSymbolAt('constant', [14, 19])).toBeTruthy();
+    expect(doc.hasSymbolAt('constant', [14, 36])).toBeTruthy();
+    expect(doc.hasSymbolAt('constant', [14, 45])).toBeTruthy();
+  });
+
+  it('should detect template strings', function() {
+    expect(doc.hasSymbolAt('string', [14, 6])).toBeTruthy();
+    expect(doc.hasSymbolAt('string', [14, 28])).toBeTruthy();
+    expect(doc.hasSymbolAt('string', [14, 34])).toBeFalsy();
+    expect(doc.hasSymbolAt('string', [14, 51])).toBeFalsy();
+  });
+
+  it('should detect escaped characters in template strings', function() {
+    expect(doc.hasSymbolAt('escaped', [14, 48])).toBeTruthy();
+    expect(doc.hasSymbolAt('escaped', [14, 49])).toBeTruthy();
+  });
+
+  it('should recognize functions and their parameters', function() {
+    var tagFunctionState = doc.getState([9, 36]).state;
+    expect(tagFunctionState.context.name).toBe('tag');
+    expect('strings' in tagFunctionState.context.params).toBeTruthy();
+    expect('values' in tagFunctionState.context.params).toBeTruthy();
+  });
+
+  it('should recognize arrow functions and their parameters', function() {
+    var arrowState = doc.getState([16, 32]).state;
+    expect('a' in arrowState.context.params).toBeTruthy();
+    expect('b' in arrowState.context.params).toBeTruthy();
+    expect('c' in arrowState.context.params).toBeTruthy();
+  });
+
   it('should toggle comments', function() {
     doc.carets[0].position(2, 4);
     cp.exec('toggleComment');
@@ -59,7 +126,7 @@ describe('JavaScript', function() {
     cp.exec('toggleComment');
     expect(doc.textAt(2)).toBe('    return 1;');
   });
-  
+
   it('should toggle block comments', function() {
     doc.carets[0].setSelectionRange({ from: [1, 2], to: [3, 3] });
     cp.exec('toggleBlockComment');
